341ac6dd00f912d28b367f6fa451dcaebdb53cc5
Julian Cruz <julian.cruz1@udea.edu.co>
2020-09-08 09:37:01 -0500

    ultimos cambios, colores, submit

---
+++ assets/custom/enviar.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    $("#formulario").bind("submit",function(){
+        // Capturamnos el boton de envío
+        var btnEnviar = $("#btnEnviar");        
+        $.ajax({
+            type: $(this).attr("method"),
+            url: $(this).attr("action"),
+            data:$(this).serialize(),
+            beforeSend: function(){
+                /*
+                * Esta función se ejecuta durante el envió de la petición al
+                * servidor.
+                * */
+                // btnEnviar.text("Enviando"); Para button 
+                btnEnviar.val("Enviando"); // Para input de tipo button
+                btnEnviar.attr("disabled","disabled");
+            },
+            complete:function(data){
+                /*
+                * Se ejecuta al termino de la petición
+                * */
+                btnEnviar.val("Enviar");
+                btnEnviar.removeAttr("disabled");
+            },
+            success: function(data){
+                /*
+                * Se ejecuta cuando termina la petición y esta ha sido
+                * correcta
+                * */
+               document.getElementById("enviado").removeAttribute("hidden");
+                //$(".respuesta").html(data);
+            },
+            error: function(data){
+                /*
+                * Se ejecuta si la peticón ha sido erronea
+                * */
+               document.getElementById("enviado").removeAttribute("hidden");
+                
+            }
+        });
+        // Nos permite cancelar el envio del formulario
+        return false;
+    });
+});
